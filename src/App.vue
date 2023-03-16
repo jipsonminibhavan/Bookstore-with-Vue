3c1751632fb9ace7e6da26509f453fa2f5d06156
Jipson Minibhavan <jipson.minibhavan@googlemail.com>
2023-03-16 14:59:03 +0100

    commit header

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,17 @@
 <template>
-  <nav class="nav justify-content-center">
-    <router-link to="/new-book">New Book</router-link>|
-    <router-link to="/book-list">Booklist</router-link>|
-    <router-link :to="{ name: 'fav-list' }">Favoriten</router-link>|
-  </nav>
+  <header class="header">
+    <section class="header__section">
+      <p class="header__task-name">📚📖👀 Bookstore</p>
+      <div class="header__bootcamp-info-wrapper">
+        <nav class="nav justify-content-center">
+          <router-link to="/new-book">New Book</router-link>|
+          <router-link to="/book-list">Booklist</router-link>|
+          <router-link :to="{ name: 'fav-list' }">Favoriten</router-link>|
+        </nav>
+      </div>
+    </section>
+  </header>
+
   <router-view />
 </template>
 <script>
@@ -13,3 +21,19 @@ export default {
   },
 };
 </script>
+<style scoped>
+.header {
+  background-color: rgb(29, 237, 199);
+  color: #fff;
+  text-align: center;
+  margin-bottom: 40px;
+}
+.header__task-name {
+  font-size: 50px;
+
+  margin: 0;
+}
+.header__section {
+  padding: 10px 10px;
+}
+</style>
